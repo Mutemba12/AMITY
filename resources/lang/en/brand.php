b23182af6031f7a9e2f92cdf97c556cbfb84169f
Temba <landustemba@gmail.com>
2022-09-17 16:49:53 +0300

    first commit

---
+++ resources/lang/en/brand.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Brand Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Brand CRUD operations.
+    |
+    */
+
+    'brands' => 'Estates',
+    'manage_your_brands' => 'Manage your estates',
+    'all_your_brands' => 'All your estates',
+    'note' => 'Note',
+    'brand_name' => 'Estate name',
+    'short_description' => 'Short description',
+    'added_success' => 'Estate added successfully',
+    'updated_success' => 'Estate updated successfully',
+    'deleted_success' => 'Estate deleted successfully',
+    'add_brand' => 'Add Estate',
+    'edit_brand' => 'Edit Estate',
+
+];
---
+++ resources/lang/en/product.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Product Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Product related operations.
+    |
+    */
+
+    'exc_of_tax' => 'Exc. tax',
+    'inc_of_tax' => 'Inc. tax',
+    'profit_percent' => 'x Margin(%)',
+    'add_new_product' => 'Add new plot',
+    'add_product_for_labels' => 'Add plot to generate Labels',
+    'product_type' => 'Plot Type',
+    'category' => 'Category',
+    'sub_category' => 'Sub category',
+    'unit' => 'Unit',
+    'brand' => 'Estate Name',
+    'tax' => 'Tax',
+    'sku' => 'SKU',
+    'alert_quantity' => 'Alert quantity',
+    'product_name' => 'Plot Number',
+    'auto_generate' => 'Auto generate',
+    'manage_stock' => 'Manage Stock?',
+    'enable_stock_help' => 'Enable stock management at plot level',
+    'barcode_type' => 'Barcode Type',
+    'applicable_tax' => 'Applicable Tax',
+    'selling_price_tax_type' => 'Selling Price Tax Type',
+    'inclusive' => 'Inclusive',
+    'exclusive' => 'Exclusive',
+    'edit_product' => "Edit Product",
+    'default_purchase_price' => 'Default Purchase Price',
+    'default_selling_price' => 'Default Selling Price',
+    'value' => 'Value',
+    'variation_name' => 'Variation Name',
+    'variation_values' => 'Variation Values',
+    'use_template' => 'Use Template',
+    'variation_values' => 'Variation Values',
+    'add_variation' => 'Add Variation',
+    'product_added_success' => 'Plot added successfully',
+    'product_updated_success' => 'Plot updated successfully',
+    'enable_product_expiry' => 'Enable Plot Expiry',
+    'expiry_period' => 'Expiry Period',
+    'expires_in' => 'Expires in',
+    'not_applicable' => 'Not Applicable',
+    'months' => 'Months',
+    'days' => 'Days',
+    'mfg_date' => 'MFG Date',
+    'exp_date' => 'EXP Date',
+    'view_product' => 'View Plot',
+    'add_product' => 'Add Plot',
+    'variations' => 'Variations',
+    'import_products' => 'Import Plot',
+    'file_to_import' => 'File To Import',
+    'file_imported_successfully' => 'File imported successfully'
+ ];
